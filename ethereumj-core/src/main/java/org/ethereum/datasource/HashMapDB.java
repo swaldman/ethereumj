2ae88f51185c088e75d89c3796a6d6829ea5aae3
Steve Waldman <swaldman@mchange.com>
2015-03-18 06:11:42 -0700

    Completed implementation of HashMapDB.

--- ethereumj-core/src/main/java/org/ethereum/datasource/HashMapDB.java
+++ ethereumj-core/src/main/java/org/ethereum/datasource/HashMapDB.java
@@ -4,6 +4,7 @@ import org.ethereum.db.ByteArrayWrapper;
 import org.iq80.leveldb.DBException;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -13,21 +14,19 @@ public class HashMapDB implements KeyValueDataSource {
 
 
     @Override
-    public void delete(byte[] arg0) throws DBException {
-        storage.remove(arg0);
+    public synchronized void delete(byte[] arg0) throws DBException {
+        storage.remove(new ByteArrayWrapper(arg0));
     }
 
 
     @Override
-    public byte[] get(byte[] arg0) throws DBException {
-
+    public synchronized byte[] get(byte[] arg0) throws DBException {
         return storage.get(new ByteArrayWrapper(arg0));
     }
 
 
     @Override
-    public void put(byte[] key, byte[] value) throws DBException {
-
+    public synchronized void put(byte[] key, byte[] value) throws DBException {
         storage.put(new ByteArrayWrapper(key), value);
     }
 
@@ -36,28 +35,31 @@ public class HashMapDB implements KeyValueDataSource {
      *
      * @return int
      */
-    public int getAddedItems() {
+    public synchronized int getAddedItems() {
         return storage.size();
     }
 
     @Override
-    public void init() {
-
+    public synchronized void init() {
     }
 
     @Override
-    public void setName(String name) {
-
+    public synchronized void setName(String name) {
     }
 
     @Override
     public Set<byte[]> keys() {
-        return null;
+	Set<ByteArrayWrapper> wrappers = storage.keySet();
+	Set<byte[]> out = new HashSet<>();
+	for ( ByteArrayWrapper wrapper : wrappers )
+	    out.add( wrapper.getData() );
+	return out;
     }
 
     @Override
     public void updateBatch(Map<byte[], byte[]> rows) {
-
+	for ( Map.Entry<byte[],byte[]> entry : rows.entrySet() )
+	    this.put( entry.getKey(), entry.getValue() );
     }
 
     @Override
